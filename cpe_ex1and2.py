63f1e8736447a28d7ac59f6847ed26d96ae7099e
Ngozi Inyama <goz@gozivision.com>
2017-06-18 17:04:32 -0700

    Add files via upload
    Modified to include dictionary in exercise 1 and to call linkmkr1function into exercise 2's linkmkr2 function and add loop into linkmkr2.

--- cpe_ex1and2.py
+++ cpe_ex1and2.py
@@ -1,41 +1,38 @@
 # Coding For Product Assessment - Exercises 1 & 2
 # Ngozi Inyama - Submitted June 18, 2017
-# ver 1.0
+# ver 1.1
 
 
 #  Exercise 1
 
 print "Exercise 1 Output"
 
-def linkmkr1(title, link):
-	if len(title) > 50:
-		short = title[0:50] + "..."
+def linkmkr1(link1):
+	if len(link1["title"]) > 50:
+		short = link1["title"][0:50] + "..."
 	else:
-		short = title
-	print "The link is: <a href=\"%s\">%s</a>" % (link, short)
+		short = link1["title"]
+	print "The link is: <a href=\"%s\">%s</a>" % (link1["url"], short)
 	
 
-rat = linkmkr1("this is a really really long title that had to be made up for testing","example.com")
-
+rat = linkmkr1({'title': 'This is a really really long title that bees had to be made up for testing', 'url': 'example.com'})
 
 print
+
 
 # Exercise 2
 
 print "Exercise 2 Output"
 
 def linkmkr2(link2):
-	if len(link2["title"]) > 50:
-		short = link2["title"][0:50] + "..."
-	else:
-		short = link2["title"]
-	print "The link is: <a href=\"%s\">%s</a>" % (link2["url"], short)
+	for x in link2:
+		linkmkr1(x)
 
 mouse = [
-		{'title': "This is a really really long title that had to be made up for testing", 'url': 'example.com'}, 
-		{'title': 'Google', 'url': 'google.com'},
+		{'title': 'This is a really really long title that had to be made up for testing', 'url': 'example.com'},
+		{'title': 'Google', 'url': 'google.com'}, 
 		{'title': 'Github', 'url': 'github.com'}
 		]
+		
 
-for x in mouse:
-	linkmkr2(x)
+gopher = linkmkr2(mouse)
